d5a0bcf3104e1940ba8baa92f4f9d0628d6d0ca2
haolinli96 <lihaolin@u.northwestern.edu>
2019-12-12 22:06:54 -0600

    use existing model for detection and example feature of each layer

--- soft_bio_use_model.py
+++ soft_bio_use_model.py
@@ -1,7 +1,4 @@
-#coding=utf-8
- 
-#使用保存好的模型
-
+# using existing model
 import tensorflow as tf
 import numpy as np
 import  data_prep as train_data
@@ -17,7 +14,7 @@ input_image = train_data.images[0:1]
 labels = train_data.labels[0:1]
 fig2,ax2 = plt.subplots(figsize=(2,2))
 ax2.imshow(np.reshape(input_image, (224, 224, 3)))
-plt.show()
+plt.savefig('input.png')
 
 
 sess = tf.Session()
@@ -95,62 +92,98 @@ fig, ax = plt.subplots(nrows=1,ncols=16,figsize=(16,1))
 for i in range(16):
     ax[i].imshow(r_tranpose_1[i][0])
 plt.title('Conv1 32*112*112')
-plt.show()
+plt.savefig('Conv1.png')
 
-#conv1 特征
-r1_relu = sess.run(relu_1,feed_dict)
-r1_tranpose = sess.run(tf.transpose(r1_relu,[3,0,1,2]))
+# layer2 visualization
+r_bn_relu_2m = sess.run(bn_relu_2m, feed_dict)
+r_tranpose_2m = sess.run(tf.transpose(r_bn_relu_2m,[3,0,1,2]))
+fig, ax = plt.subplots(nrows=1,ncols=16,figsize=(16,1))
+for i in range(16):
+    ax[i].imshow(r_tranpose_2m[i][0])
+plt.title('Depthwise_Conv2 32*112*112')
+plt.savefig('Depthwise_Conv2.png')
+r_bn_relu_2f = sess.run(bn_relu_2f, feed_dict)
+r_tranpose_2f = sess.run(tf.transpose(r_bn_relu_2f,[3,0,1,2]))
+fig, ax = plt.subplots(nrows=1,ncols=16,figsize=(16,1))
+for i in range(16):
+    ax[i].imshow(r_tranpose_2f[i][0])
+plt.title('1*1_Conv2 64*112*112')
+plt.savefig('Conv2.png')
+
+# layer3 first MBNet visualization
+r_bn_relu_3m = sess.run(bn_relu_3m, feed_dict)
+r_tranpose_3m = sess.run(tf.transpose(r_bn_relu_3m,[3,0,1,2]))
+fig, ax = plt.subplots(nrows=1,ncols=16,figsize=(16,1))
+for i in range(16):
+    ax[i].imshow(r_tranpose_3m[i][0])
+plt.title('Depthwise_Conv3 64*56*56')
+plt.savefig('Depthwise_Conv3.png')
+r_bn_relu_3f = sess.run(bn_relu_3f, feed_dict)
+r_tranpose_3f = sess.run(tf.transpose(r_bn_relu_3f,[3,0,1,2]))
+fig, ax = plt.subplots(nrows=1,ncols=16,figsize=(16,1))
+for i in range(16):
+    ax[i].imshow(r_tranpose_3f[i][0])
+plt.title('1*1_Conv3 128*56*56')
+plt.savefig('Conv3.png')
+
+# layer4 first MBNet visualization
+r_bn_relu_4m = sess.run(bn_relu_4m, feed_dict)
+r_tranpose_4m = sess.run(tf.transpose(r_bn_relu_4m,[3,0,1,2]))
+fig, ax = plt.subplots(nrows=1,ncols=16,figsize=(16,1))
+for i in range(16):
+    ax[i].imshow(r_tranpose_4m[i][0])
+plt.title('Depthwise_Conv4 128*28*28')
+plt.savefig('Depthwise_Conv4.png')
+r_bn_relu_4f = sess.run(bn_relu_4f, feed_dict)
+r_tranpose_4f = sess.run(tf.transpose(r_bn_relu_4f,[3,0,1,2]))
+fig, ax = plt.subplots(nrows=1,ncols=16,figsize=(16,1))
+for i in range(16):
+    ax[i].imshow(r_tranpose_4f[i][0])
+plt.title('1*1_Conv4 256*28*28')
+plt.savefig('Conv4.png')
+
+
+# layer5 first MBNet visualization
+r_bn_relu_5m = sess.run(bn_relu_5m, feed_dict)
+r_tranpose_5m = sess.run(tf.transpose(r_bn_relu_5m,[3,0,1,2]))
+fig, ax = plt.subplots(nrows=1,ncols=16,figsize=(16,1))
+for i in range(16):
+    ax[i].imshow(r_tranpose_5m[i][0])
+plt.title('Depthwise_Conv5 256*14*14')
+plt.savefig('Depthwise_Conv5.png')
+r_bn_relu_5f = sess.run(bn_relu_5f, feed_dict)
+r_tranpose_5f = sess.run(tf.transpose(r_bn_relu_5f,[3,0,1,2]))
+fig, ax = plt.subplots(nrows=1,ncols=16,figsize=(16,1))
+for i in range(16):
+    ax[i].imshow(r_tranpose_5f[i][0])
+plt.title('1*1_Conv5 512*14*14')
+plt.savefig('Conv5.png')
+
+# layer6 first MBNet visualization
+r_bn_relu_6m = sess.run(bn_relu_6m, feed_dict)
+r_tranpose_6m = sess.run(tf.transpose(r_bn_relu_6m,[3,0,1,2]))
+fig, ax = plt.subplots(nrows=1,ncols=16,figsize=(16,1))
+for i in range(16):
+    ax[i].imshow(r_tranpose_6m[i][0])
+plt.title('Depthwise_Conv6 512*7*7')
+plt.savefig('Depthwise_Conv6.png')
+r_bn_relu_6f = sess.run(bn_relu_6f, feed_dict)
+r_tranpose_6f = sess.run(tf.transpose(r_bn_relu_6f,[3,0,1,2]))
+fig, ax = plt.subplots(nrows=1,ncols=16,figsize=(16,1))
+for i in range(16):
+    ax[i].imshow(r_tranpose_6f[i][0])
+plt.title('1*1_Conv6 1024*7*7')
+plt.savefig('Conv6.png')
+
+
+# 7 * 7 pool
+r_avg_pool_7 = sess.run(avg_pool_7,feed_dict)
+r_tranpose_7 = sess.run(tf.transpose(r_avg_pool_7,[3,0,1,2]))
 fig,ax = plt.subplots(nrows=1,ncols=16,figsize=(16,1))
 for i in range(16):
-    ax[i].imshow(r1_tranpose[i][0])
-plt.title('Conv1 16*112*92')
-plt.show()
-
-#pool1特征
-max_pool_1 = sess.run(max_pool_1,feed_dict)
-r1_tranpose = sess.run(tf.transpose(max_pool_1,[3,0,1,2]))
-fig,ax = plt.subplots(nrows=1,ncols=16,figsize=(16,1))
-for i in range(16):
-    ax[i].imshow(r1_tranpose[i][0])
-plt.title('Pool1 16*56*46')
-plt.show()
+    ax[i].imshow(r_tranpose_7[i][0])
+plt.title('7*7_Avg_Pool7 1024*1*1')
+plt.savefig('Avg_Pool.png')
 
 
-#conv2 特征
-r2_relu = sess.run(relu_2,feed_dict)
-r2_tranpose = sess.run(tf.transpose(r2_relu,[3,0,1,2]))
-fig,ax = plt.subplots(nrows=1,ncols=32,figsize=(32,1))
-for i in range(32):
-    ax[i].imshow(r2_tranpose[i][0])
-plt.title('Conv2 32*56*46')
-plt.show()
-
-#pool2 特征
-max_pool_2 = sess.run(max_pool_2,feed_dict)
-tranpose = sess.run(tf.transpose(max_pool_2,[3,0,1,2]))
-fig,ax = plt.subplots(nrows=1,ncols=32,figsize=(32,1))
-for i in range(32):
-    ax[i].imshow(tranpose[i][0])
-plt.title('Pool2 32*28*23')
-plt.show()
-
-
-#conv3 特征
-r3_relu = sess.run(relu_3,feed_dict)
-tranpose = sess.run(tf.transpose(r3_relu,[3,0,1,2]))
-fig,ax = plt.subplots(nrows=1,ncols=64,figsize=(32,1))
-for i in range(64):
-    ax[i].imshow(tranpose[i][0])
-plt.title('Conv3 64*28*23')
-plt.show()
-
-#pool3 特征
-max_pool_3 = sess.run(max_pool_3,feed_dict)
-tranpose = sess.run(tf.transpose(max_pool_3,[3,0,1,2]))
-fig,ax = plt.subplots(nrows=1,ncols=64,figsize=(32,1))
-for i in range(64):
-    ax[i].imshow(tranpose[i][0])
-plt.title('Pool3 64*14*12')
-plt.show()
-
-print sess.run(f_softmax,feed_dict)
+print (sess.run(f_softmax,feed_dict))
